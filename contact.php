f55b21532c9a9904f4d98f957853fd1ebf953385
Andreas Vasd <andreas_1996@windowslive.com>
2018-04-06 21:35:54 +0300

    Update contact.php

--- contact.php
+++ contact.php
@@ -17,10 +17,10 @@ try {
     //Server settings
     $mail->SMTPDebug = 2;                                 // Enable verbose debug output
     $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'smtp-mail.outlook.com';                        // Specify main and backup SMTP servers
+    $mail->Host = 'smtp-mail.outlook.com';                // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'andreas_vasdekis@windowslive.com';                 // SMTP username
-    $mail->Password = 'Parlapip@s';                       // SMTP password
+    $mail->Username = 'andreas_vasdekis@windowslive.com';  // SMTP username
+    $mail->Password = '******';                            // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;                                    // TCP port to connect to
     
